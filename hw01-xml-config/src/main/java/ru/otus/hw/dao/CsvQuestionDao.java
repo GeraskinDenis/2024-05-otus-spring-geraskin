410b23918ab767adc514cc8d424a9a855eca17c0
Geraskin Denis <sun7smile@ya.ru>
2024-06-29 22:19:13 +0300

    hw01: Fix checkstyle

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -21,13 +21,8 @@ public class CsvQuestionDao implements QuestionDao {
 
 	@Override
 	public List<Question> findAll() {
-		// Использовать CsvToBean
-		// https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-		// Использовать QuestionReadException
-		// Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 		List<Question> questions;
 		InputStream inputStream = getFileAsStream(fileNameProvider.getTestFileName());
-
 		try (InputStreamReader streamReader = new InputStreamReader(inputStream);
 			 BufferedReader reader = new BufferedReader(streamReader)) {
 			questions = new CsvToBeanBuilder<QuestionDto>(reader)
