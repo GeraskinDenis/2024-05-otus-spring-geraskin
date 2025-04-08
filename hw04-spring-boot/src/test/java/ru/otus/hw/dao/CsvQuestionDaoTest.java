9ce4ae5051607fe6cb1c84b4e4255d3ffe9aeba6
Geraskin Denis <sun7smile@ya.ru>
2025-04-08 20:22:16 +0300

    hw04: added `CsvQuestionDaoTest.java`

---
+++ hw04-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -0,0 +1,45 @@
+package ru.otus.hw.dao;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class CsvQuestionDaoTest {
+
+    @Mock
+    private TestFileNameProvider testFileNameProvider;
+
+    @Autowired
+    private CsvQuestionDao questionDao;
+
+    @BeforeEach
+    public void beforeEach() {
+        when(testFileNameProvider.getTestFileName()).thenReturn("questions.csv");
+    }
+
+    @Test
+    @DisplayName("Should have find all questions correctly")
+    public void shouldFindAllCorrectlyEn() {
+        List<Answer> answers = Arrays.asList(new Answer("Answer1", false),
+                new Answer("Answer2", false),
+                new Answer("Answer3", true));
+        Question expectedQuestion = new Question("Question text?", answers);
+
+        List<Question> questionList = questionDao.findAll();
+        Question dummyQuestion = new Question("Dummy question", Arrays.asList(new Answer("Dummy answer", true)));
+        assertThat(questionList).isNotNull()
+                .isNotEmpty().contains(expectedQuestion)
+                .hasSize(1).doesNotContain(dummyQuestion);
+    }
+}
